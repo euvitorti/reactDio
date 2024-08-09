0b82ce59ec77aabe01784c071bfc4e7ddd19cade
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-08-09 11:15:13 -0300

    Inserção de imagens

--- assets/cfb/src/App.js
+++ assets/cfb/src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
 
+// APONTANDO PARA A PASTA EM SRC
+import Bob from './components/img/bob.jpeg'
+
 export default function App() {
-  return(
+
+  const canal = 'CFB Cursos';
+  const curso = 'React';
+  const autor = 'Bruno';
+
+  return (
     <>
-    <p>CFB CURSOS</p>
+      <p>Canal: {canal}</p>
+      <p>Curso: {curso}</p>
+      <p>Autor: {autor}</p>
+      <img src={Bob} width={500} height={300}></img>
+      {/* APONTANDO PARA A PASTA EM PUBLIC */}
+      <img src="img/sid.jpeg" width={500} height={300}></img>
     </>
   )
 }
